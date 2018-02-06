14b9f83ed65f8d85c34f41228e8c284ced14021a
M-Agi <agnes.macsotai@gmail.com>
2018-02-06 20:32:05 +0100

    Fix infinite loop

--- roller_game.py
+++ roller_game.py
@@ -3,7 +3,6 @@
 import menus
 import simulation
 
-
 input_build_building = "1"
 input_upgrade_building = "2"
 input_buy_advertisement = "3"
@@ -11,7 +10,8 @@ input_employ = "4"
 input_end_turn = "5"
 input_exit = "9"
 
-selectable_items = [input_build_building, input_upgrade_building, input_buy_advertisement, input_employ, input_end_turn, input_exit]
+selectable_items = [input_build_building, input_upgrade_building, input_buy_advertisement, input_employ, input_end_turn,
+                    input_exit]
 screen_action_menu = False
 screen_build_menu = False
 screen_upgrade_menu = False
@@ -30,24 +30,22 @@ while True:
         if game_mode == "9":
             break
         else:
-            while selected_menu not in selectable_items and game_mode == 1:
-                selected_menu = raw_input("Select an item from the menu")
+            while selected_menu not in selectable_items and game_mode == "1":
                 menus.menu()
+                selected_menu = raw_input("Select an item from the menu")
                 if selected_menu == input_build_building:
-                     menus.building_menu(my_park)
-
+                    menus.building_menu(my_park)
                 elif selected_menu == input_upgrade_building:
-                     if len(my_park.buildings) != 0:
-                         menus.upgrade_menu()
-                     else:
-                         selected_menu = raw_input("You have no buildings to upgrade")
+                    if len(my_park.buildings) != 0:
+                        menus.upgrade_menu()
+                    else:
+                        selected_menu = raw_input("You have no buildings to upgrade")
                 elif selected_menu == input_buy_advertisement:
-                     pass #TODO
+                    pass  # TODO
                 elif selected_menu == input_employ:
-                     pass #TODO
+                    pass  # TODO
                 elif selected_menu == input_exit:
-                     break
+                    break
     break
-
 
 my_park.turn()
